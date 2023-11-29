1a1d7329486b93c798a53ddd8f0b4f80a8cdef84
lorenaclaeys <lorena21novembre@gmail.com>
2023-11-29 10:19:04 -0500

    add of prostate phantom

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -6,33 +6,37 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 import xacro
 
-
 def generate_launch_description():
+    # Define the 'use_sim_time' launch configuration variable
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    # Get the path to the 'my_bot' package and the xacro files
+    pkg_path = get_package_share_directory('my_bot')
+    xacro_file_robot = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+
+    # Process the xacro files into URDF descriptions
+    robot_description_config = xacro.process_file(xacro_file_robot)
+ 
+    # Create the robot_state_publisher nodes with the necessary parameters
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{'robot_description': robot_description_config.toxml(),
+                     'use_sim_time': use_sim_time}],
     )
 
 
-    # Launch!
+    # Define the launch description with a declaration for 'use_sim_time'
+    # and the robot_state_publisher nodes
     return LaunchDescription([
         DeclareLaunchArgument(
-            'use_sim_time',
+            name='use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
+            description='Flag to enable use_sim_time'
+        ),
+        node_robot_state_publisher,
     ])
+
+
